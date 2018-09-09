bf6be10e27291bfe0def7f8133ba15f35ece41d6
Marios Papachristou <papachristoumarios@gmail.com>
2018-09-09 22:34:54 +0300

    Change project title

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
+import setuptools
 from distutils.core import setup
 
-setup(name='Gomory-Hu tree Data Structure Algorithm  for Python',
+setup(name='python-GomoryHu',
       version='1.0',
-      description='Gomory-Hu tree Data Structure Algorithm  for Python with Gusfield Algorithm',
+      description='Gomory-Hu tree Data Structure Algorithm for Python with Gusfield Algorithm',
       author='Marios Papachristou',
       author_email='papachristoumarios@gmail.com',
       url='https://github.com/papachristoumarios/python-GomoryHu',
